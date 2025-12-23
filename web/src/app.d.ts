b5353174daf13c956388018b0d3f08dc21c7ba6b
yamancan <yamancandev@gmail.com>
2025-12-24 02:50:49 +0300

    config(web): init workspace config and types

---
+++ web/src/app.d.ts
@@ -0,0 +1,28 @@
+/**
+ * Global type declarations for Zarf web application
+ */
+
+/// <reference types="@sveltejs/kit" />
+/// <reference types="vite/client" />
+
+// Ethereum provider (MetaMask, etc.)
+interface Window {
+	ethereum?: {
+		request: (args: { method: string; params?: any[] }) => Promise<any>;
+		on: (event: string, callback: (...args: any[]) => void) => void;
+		removeListener: (event: string, callback: (...args: any[]) => void) => void;
+	};
+}
+
+// Environment variables
+interface ImportMetaEnv {
+	readonly VITE_GOOGLE_CLIENT_ID?: string;
+	readonly VITE_VESTING_ADDRESS?: string;
+	readonly VITE_JWK_REGISTRY_ADDRESS?: string;
+	readonly VITE_RPC_URL?: string;
+	readonly VITE_NETWORK?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
