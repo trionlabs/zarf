99e2973197f5c6f78c08d75f8a5c6047ed17954e
0x471 <0x471@protonmail.com>
2026-04-27 22:27:10 +0300

    remove video folder

--- video/src/components/MockUI/index.ts
+++
@@ -1,6 +0,0 @@
-export { MockCard } from "./MockCard";
-export { MockInput } from "./MockInput";
-export { MockButton } from "./MockButton";
-export { MockTimeline } from "./MockTimeline";
-export { MockBadge } from "./MockBadge";
-export { MockProof } from "./MockProof";
--- video/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as GrainOverlay } from "./GrainOverlay";
-export { default as FingerprintRipples } from "./FingerprintRipples";
-export { default as ZarfLogo } from "./ZarfLogo";
-export { default as CameraMotion } from "./CameraMotion";
-export { Animated } from "./Animated";
-export { GlowBurst } from "./GlowBurst";
